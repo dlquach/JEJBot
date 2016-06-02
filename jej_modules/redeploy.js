d56ec219a5260f5bde45ac8d7ee0c066c71f49ab
dlquach <derekq.alt@gmail.com>
2016-06-01 19:50:00 -0700

    Fixed redeploy

--- jej_modules/redeploy.js
+++ jej_modules/redeploy.js
@@ -2,7 +2,7 @@
  * Does a pull which should trigger the nodemon to restart the app.
  */
 var redeployHandler = function(client, channel) {
-    require('simple-git').pull();
+    require('simple-git')().pull();
     client.sendMessage(channel, "Pulled from master.");
 }
 

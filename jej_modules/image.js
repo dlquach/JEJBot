8a77e7832face49a0dd70b1eb73a5871ff3e8461
dlquach <derekq.alt@gmail.com>
2016-06-01 21:25:30 -0700

    Fixed image handler crashing on empty query

--- jej_modules/image.js
+++ jej_modules/image.js
@@ -4,6 +4,9 @@ const googleImages = require('google-images');
 var imageClient = googleImages(keys.cseID, keys.apiKey);
 
 var imageHandler = function (client, channel, content) {
+    if (!content) {
+        client.sendMessage(channel, "Usage: !image <query>");
+    }
     console.log("Searching for: ", content);
     imageClient.search(content)
         .then(function (images, anything) {

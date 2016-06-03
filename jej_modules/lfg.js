beb4288e954f1abc244de39d085d6a985d3eb430
Yuto Otaguro <yotagur@gmail.com>
2016-06-03 01:21:51 -0500

    Basic LFG functionality implemented.

--- jej_modules/lfg.js
+++ jej_modules/lfg.js
@@ -1,16 +1,58 @@
 "use strict";
 
-var gamesSupported = require('../jej_modules/tools/game_list').pretty();
+var games = require('../jej_modules/tools/games_supported');
 
 var GameList = require('../jej_modules/tools/gamelist');
-
 var gameList = new GameList();
 
-var lfgHandler = function(client, channel, _, message) {
-    var user = message.author.id;
+/**
+ * Parse the message sent by the user. This will simply split into two tokens separated by a space.
+ * If 
+ */
+function parseContent(content) {
+    // If empty content, show list.
+    if (content === undefined) {
+        return "";
+    }
 
-    
-    client.sendMessage(channel, "Invalid game. Here's what's available:\n" + gamesSupported);
+    var tokens = content.split(' ');
+    var mins = parseInt(tokens[tokens.length - 1]);
+
+    // Token component validation.
+    if (isNaN(mins)) {
+        return undefined;
+    }
+
+    // Put together the name of the game before returning.
+    var gameName = (tokens.slice(0, tokens.length - 1)).join(' ');
+
+    return {
+        'game': gameName,
+        'time': mins
+    };
+}
+
+var lfgHandler = function(client, channel, content, message) {
+    var username = message.author.username + '#' + message.author.discriminator;
+
+    // Parse the content and see if it's valid.
+    var tokens = parseContent(content);
+
+    if (tokens === undefined) {
+        client.sendMessage(channel, "Correct usage: !lfg <Game> <Minutes Available> OR simply !lfg to see LFG list.");
+        return;
+    }
+   
+    // Empty string input means show the list. 
+    if (tokens === "") {
+        client.sendMessage(channel, gameList.stringifyList());
+    }
+    else if (games.isSupported(tokens.game)) {
+        console.log(username + " is looking for " + tokens.game + " for " + tokens.time + " minutes.");
+        gameList.addGame(username, tokens.game);
+    } else {
+        client.sendMessage(channel, "Invalid game. Here's what's available:\n" + games.pretty());
+    }
 };
 
 module.exports = lfgHandler;
--- jej_modules/tools/games_supported.js
+++ jej_modules/tools/games_supported.js
@@ -5,16 +5,25 @@ var games = {
     'League of Legends': true
 };
 
+function isSupported(name) {
+    if (name in games) {
+        return true;
+    }
+
+    return false;
+}
+
 module.exports = {
     raw: games,
     pretty: function() {
         var stringify = '';
         for (var title in games) {
             if (games.hasOwnProperty(title) && games[title]) {
-                stringify += title + '\n';
+                stringify += " - " + title + '\n';
             }
         }
 
         return stringify;
-    }
+    },
+    isSupported: isSupported
 };

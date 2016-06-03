419194ee4f45fa5b963fb7460205bcfc61c0791a
Yuto Otaguro <yotagur@gmail.com>
2016-06-02 23:04:51 -0500

    Game list to maintain users LFG.

--- jej_modules/lfg.js
+++ jej_modules/lfg.js
@@ -1,73 +1,16 @@
 "use strict";
 
-var gameList = require('../jej_modules/tools/game_list').pretty();
+var gamesSupported = require('../jej_modules/tools/game_list').pretty();
 
-class GameList {
-    constructor() {
-        this.games = {};
-    }
-
-    addGame(playerName, game) {
-        // If the player already has a list, add to list.
-        if (playerName in this.games) {
-            this.games[playerName].push(game);
-        } else {
-            this.games[playerName] = [game];
-        }
-    }
-    
-    stringifyList() {
-        var result = '';
-        for (var user in this.games) {
-            result += user + "\n";
-        
-            // Iterate through list of games.
-            for (var game in this.games[user]) {
-                result += " - " + game + "\n";
-            }
-        }
-
-        return result;
-    }
-}
+var GameList = require('../jej_modules/tools/gamelist');
 
 var gameList = new GameList();
 
-var lfgHandler = function(client, channel, content) {
+var lfgHandler = function(client, channel, _, message) {
+    var user = message.author.id;
 
     
-
-    /*
-    var user = channel.messages['0'].author.id;
-    var roles = channel.server.roles;
-
-    // The number of roles that exist.
-    var length = parseInt(roles.length);
-
-    // Create dict of titles -> roles.
-    var games = {};
-
-    for (var i = 0; i < length; ++i) {
-        var role = roles[i];
-        
-        // If the role's name doesn't have @, it's a game
-        if ((role.name).indexOf('@') == -1) {
-            games[role.name.toLowerCase()] = role;
-        }
-    }
-   
-    removeRoleFromUser(user, channel);
-
-    if (content in games) {
-        client.addMemberToRole(user, games[content.toLowerCase()], function(error) {
-            if (error) {
-                console.log(error);
-            }
-        });
-    } else {
-        client.sendMessage(channel, "Invalid game. Here's what's available:\n" + gameList);
-    }
-    */
+    client.sendMessage(channel, "Invalid game. Here's what's available:\n" + gamesSupported);
 };
 
 module.exports = lfgHandler;

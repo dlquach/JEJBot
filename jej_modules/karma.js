4fe284c72cea38c75fa2ea05826fff7c40a92adf
dlquach <derekq.alt@gmail.com>
2016-06-07 04:30:47 -0700

    Forgot to pass in client

--- jej_modules/karma.js
+++ jej_modules/karma.js
@@ -1,7 +1,7 @@
 var karma = require('../jej_modules/tools/karma.js');
 
 var karmaHandler = function (client, channel, content) {
-    karma.printKarma(content);
+    karma.printKarma(client, content);
 }
 
 module.exports = karmaHandler;

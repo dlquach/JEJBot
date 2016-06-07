7aa915fd38febbcd112ae77b620323d4618583be
Yuto Otaguro <yotagur@gmail.com>
2016-06-07 16:50:47 -0500

    Stats ready for BETA.

--- jej_modules/stats.js
+++ jej_modules/stats.js
@@ -1,5 +1,33 @@
-var statsHandler = function (client, channel, _, message) {
+"use strict";
 
+var UserMethods = require('./tools/user_methods');
+var GameStats = require('./tools/gamestats');
+
+function statsString(stats) {
+    var result = '';
+
+    for (var game in stats) {
+        var m = Math.floor(stats[game] / 60);
+        var seconds = stats[game] % 60;
+
+        var h = Math.floor(m / 60);
+        var minutes = m % 60;
+
+        var days = Math.floor(h / 24);
+        var hours = Math.floor(h & 24);
+
+        result += game + ': ' + days + ' days ' + hours + ' hours ' + minutes + ' minutes ' + seconds + ' seconds\n';
+    }
+
+    return result;
+}
+
+var statsHandler = function (client, channel, _, message) {
+    var uniqueName = UserMethods.getUniqueName(message.author);
+
+    GameStats.getExistingTimes(uniqueName, function (stats) {
+        client.sendMessage(channel, statsString(stats));
+    });
 };
 
 module.exports = statsHandler;

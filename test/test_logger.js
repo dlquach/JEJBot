78d609cb989fd62f733a79011efb978e37fc9d6c
Yuto Otaguro <yotagur@gmail.com>
2016-06-10 16:55:28 -0500

    Logger constructed
    Instead of using console.log, first import logger from jej_modules/tools
    and the available logging levels are "log", "warn", and "error"

---
+++ test/test_logger.js
@@ -0,0 +1,15 @@
+var chai = require('chai');
+
+var it = require('mocha/lib/mocha.js').it;
+var describe = require('mocha/lib/mocha.js').describe;
+
+var expect = chai.expect;
+var should = chai.should();
+
+
+describe('Logger Test', function () {
+    it('input basic log', function (done) {
+
+        done();
+    });
+});

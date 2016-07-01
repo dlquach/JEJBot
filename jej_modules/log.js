9dcd0d4a4c3301e673130d3d297760eb7d577dd3
Yuto Otaguro <yotagur@gmail.com>
2016-06-30 19:16:41 -0500

    Got rid of any function that creates files.

--- jej_modules/log.js
+++
@@ -1,9 +0,0 @@
-var Logger = require('./tools/logger');
-
-var logHandler = function(client, channel) {
-    Logger.log("Uploading log file.");
-
-    client.sendFile(channel, 'output/bot.log');
-}
-
-module.exports = logHandler;

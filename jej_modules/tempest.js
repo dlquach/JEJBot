1d1558daa2f920024bb3e698784f7fb7ca4d9be7
dlquach <derekq.alt@gmail.com>
2018-01-12 02:17:55 -0800

    use senders voice channel directly

--- jej_modules/tempest.js
+++ jej_modules/tempest.js
@@ -1,29 +1,17 @@
 var Discord = require('discord.js');
 
 var tempestHandler = function (client, channel, content, message) {
-    for (var channel of channel.guild.channels) {
-        if (channel[1] instanceof Discord.VoiceChannel) {
-            console.log("channel")
-            // Find the voice channel that the sender is in
-            for (var member of channel[1].members) {
-                if (member[0] === message.author.id) {
-                    console.log('found');
-                    channel.join()
-                        .then(connection => {
-                            console.log(channel);
-                            return playTempest(client, channel);
-                        })
-                        .then(dispatcher => {
-                            dispatcher.on('error', console.error);
-                        })
-                        .catch(console.error);
-                }
-            }
-        }
-    }
+    const voice = message.member.voiceChannel;
+    voice.join()
+        .then(
+            connection =>
+            console.log(voice);
+            return playTempest(connection, voice);
+        })
+        .catch(console.error);
 }
 
-function playTempest(client, channel) {
+function playTempest(connection, channel) {
     connection.playFile(
         '../audio/tempest.wav',
         {

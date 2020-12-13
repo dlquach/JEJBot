3b21b1e91fe93b7cdab364335c752c36496f2cf5
Derek Quach <dlquach2@illinois.edu>
2020-12-13 23:42:19 +0000

    google image stuff in tools

--- jej_modules/image.js
+++ jej_modules/image.js
@@ -1,31 +1,7 @@
-var keys = require('../credentials/keys.js');
-var Perms = require('./tools/image-permissions');
-
-const googleImages = require('google-images');
-var imageClient = new googleImages(keys.cseID, keys.apiKey);
+var googleImageSearch = require('../jej_modules/tools/google-image-search.js');
 
 var imageHandler = function (client, channel, content) {
-    if (!content) {
-        channel.send("Usage: !image <query>");
-        return;
-    }
-    console.log("Searching for: ", content);
-    imageClient.search(content)
-        .then(function (images, anything) {
-            let randomIndex = 0;
-            
-            // Save this bot from overwork :(
-            let cycle = 0;
-
-            console.log("Num results: ", images.length);
-            do {
-                randomIndex = Math.floor((Math.random() * images.length));
-                console.log("Random index: ", randomIndex);
-                cycle++;
-            } while (Perms.domainIsBlocked(images[randomIndex]['url']) && cycle < 100)
-
-            channel.send(images[randomIndex]['url']);
-        });
+    googleImageSearch.search(client, channel, content, {type: 'animated'});
 }
 
 module.exports = imageHandler;
---
+++ jej_modules/tools/google-image-search.js
@@ -0,0 +1,34 @@
+var keys = require('../../credentials/keys.js');
+var Perms = require('./image-permissions');
+
+const googleImages = require('google-images');
+var imageClient = new googleImages(keys.cseID, keys.apiKey);
+
+// options needs to be a dict
+// ex. {type: 'animated'}
+var search = function (client, channel, content, options) {
+    if (!content) {
+        channel.send("Usage: !gif <query>");
+        return;
+    }
+    console.log("Searching for: ", content);
+    imageClient.search(content, options)
+        .then(function (images, anything) {
+            let randomIndex = 0;
+            
+            // Save this bot from overwork :(
+            let cycle = 0;
+
+            console.log("Num results: ", images.length);
+            do {
+                randomIndex = Math.floor((Math.random() * images.length));
+                console.log("Random index: ", randomIndex);
+                console.log("url: ", images[randomIndex]['url']);
+                cycle++;
+            } while (Perms.domainIsBlocked(images[randomIndex]['url']) && cycle < 100)
+
+            channel.send(images[randomIndex]['url']);
+        });
+}
+
+module.exports.search = search; 

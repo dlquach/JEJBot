006f83d4ca6eb3a48701223bc40b307c9b5976b1
Goyatuzo <yotagur@gmail.com>
2020-12-17 15:14:40 -0600

    Allow folders to be modules

--- jej_modules/index.js
+++ jej_modules/index.js
@@ -1,7 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+function isModule(fname) {
+    // A js file is defaulted as a module.
+    if (fname.match(/\.js$/) !== null && fname !== 'index.js') {
+        return true;
+    }
+
+    // If the module exists inside a folder, check for an index.js file to import.
+    const modulePath = path.join(__dirname, fname);
+    if (fs.lstatSync(modulePath).isDirectory()) {
+        for (const file of fs.readdirSync(modulePath)) {
+            if (file === 'index.js') {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
 // Lifted from StackOverflow http://stackoverflow.com/questions/5364928/node-js-require-all-files-in-a-folder
-require('fs').readdirSync(__dirname + '/').forEach(function(file) {
-  if (file.match(/\.js$/) !== null && file !== 'index.js') {
-    var name = file.replace('.js', '');
-    exports[name] = require('./' + file);
-  }
+fs.readdirSync(__dirname + '/').forEach(function (file) {
+    if (isModule(file)) {
+        var name = file.replace('.js', '');
+        exports[name] = require('./' + file);
+    }
 });

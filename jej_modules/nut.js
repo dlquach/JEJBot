305007d30fa58f062bd15ac5d75a8d979ef06052
Derek Quach <dlquach2@illinois.edu>
2020-12-19 20:09:32 +0000

    the ceo of geo is here

---
+++ jej_modules/nut.js
@@ -0,0 +1,5 @@
+var nutHandler = function(client, channel) {
+    channel.send("https://media.discordapp.net/attachments/507800980855717889/789781345189756938/zhongstick.gif");
+}
+
+module.exports = nutHandler;

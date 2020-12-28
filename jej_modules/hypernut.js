99c111b3bc17112bbcca2d20a1afcc9588d7a1d4
Derek Quach <dlquach2@illinois.edu>
2020-12-28 22:27:39 +0000

    now its hyper

---
+++ jej_modules/hypernut.js
@@ -0,0 +1,5 @@
+var hyperNutHandler = function(client, channel) {
+    channel.send("https://media.discordapp.net/attachments/766637522348408832/787334664799911956/zhongli.gif");
+}
+
+module.exports = hyperNutHandler;

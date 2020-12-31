2e0fdfb81275e08621df8376aa305b5a589435d8
Derek Quach <dlquach2@illinois.edu>
2020-12-31 08:52:54 +0000

    split 8ball responses up by sentiment

--- jej_modules/8ball.js
+++ jej_modules/8ball.js
@@ -1,20 +1,27 @@
 var eightBallHandler = function (client, channel, content, message) {
-    let fortunes = [
-        'ask again later',
+    let positiveFortunes = [
         'you may count on it',
         'as i see it, yes',
-        'not at this time',
-        'unclear',
-        'very doubtful',
-        'better not to tell you now',
-        'concentrate and ask again',
         'most likely',
-        'my sources say no',
-        'reply hazy, try again',
-        'don\'t count on it',
         'certainly',
         'indeed'
     ];
+    let negativeFortunes = [
+        'my sources say no',
+        'don\'t count on it',
+        'not at this time',
+        'very doubtful',
+        'probably not'
+    ];
+    let neutralFortunes = [
+        'ask again later',
+        'unclear',
+        'better not to tell you now',
+        'concentrate and ask again',
+        'reply hazy, try again'
+    ];
+
+    let fortunes = [].concat(positiveFortunes).concat(negativeFortunes).concat(neutralFortunes);
 
     var fortune = (Math.floor(Math.random() * fortunes.length));
     channel.send(message.author + ': ' + fortunes[fortune]);

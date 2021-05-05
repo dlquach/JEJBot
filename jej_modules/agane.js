889716d5c2c6e6c8ce062be735bc7c2edc198e36
Derek Quach <dlquach2@illinois.edu>
2021-05-05 07:46:01 +0000

    agane ignores echo to prevent infinite loops

--- jej_modules/agane.js
+++ jej_modules/agane.js
@@ -3,7 +3,7 @@ var aganeHandler = function (client, channel) {
         msgArray = messages.array();
         for (const msg in msgArray) {
             let m = msgArray[msg];
-            if (!m.author.bot && m.content[0] == '!' && m.content != '!agane') {
+            if (!m.author.bot && m.content[0] == '!' && m.content != '!agane' && !m.content.includes('!echo')) {
                 channel.send(m.content);
                 return;
             }
